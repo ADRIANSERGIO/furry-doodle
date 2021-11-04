3fb1335896cc8d0af5fbb74a16f525e9160f0ce2
ADRIANSERGIO <asdic1800@yahoo.com.ar>
2021-11-04 20:05:35 -0300

    subiendo procfile y app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => console.log("servidor ejecutándose en puerto 3000"));
+app.listen(process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "views/home.html"))
