e4ec632c861ef6e5621ba46e59c20a98c1d48a8a
ADRIANSERGIO <asdic1800@yahoo.com.ar>
2021-11-04 17:53:15 -0300

    subiendo archivos y formularios

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => console.log("servidor ejecutandose en puerto 3000"));
+app.listen(3000, () => console.log("servidor ejecutándose en puerto 3000"));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "views/home.html"))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, "views/login.html"))
 });
